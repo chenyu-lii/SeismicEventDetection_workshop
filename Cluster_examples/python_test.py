8fc87656c5b43189886a71b97fc5a0e7dc602f9e
chenyu_li <lchenyu1992@gmail.com>
2021-08-26 16:42:34 +0800

    Update python_test.py

--- Cluster_examples/python_test.py
+++ Cluster_examples/python_test.py
@@ -2,10 +2,11 @@
 PBS job test run, import a module
 '''
 
-#import eqt
-import obspy
+import EQTransformer
+#import obspy
 from datetime import datetime
 
 run_end_time=datetime.now()
-print('Obspy module imported successfully')
+#print('Obspy module imported successfully')
+print('EQTransformer module imported successfully')
 print('Finished '+str(run_end_time))
